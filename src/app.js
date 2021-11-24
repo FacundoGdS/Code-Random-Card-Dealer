21a3baf0584ce4143153f1137e699d0ae7cd0559
Facundo <facundogds@gmail.com>
2021-11-24 00:00:33 +0000

    Update-card-dealer

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = ["♦", "♥", "♠", "♣"];
+  let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  let palorandom = Math.floor(Math.random() * palos.length);
+  let numerosrandom = Math.floor(Math.random() * numeros.length);
+
+  let topPalo = document.querySelector(".top");
+  let bottomPalo = document.querySelector(".bottom");
+  let middleNumber = document.querySelector(".middle");
+
+  topPalo.innerHTML = palos[palorandom];
+  bottomPalo.innerHTML = palos[palorandom];
+  middleNumber.innerHTML = numeros[numerosrandom];
+
+  console.log(topPalo.innerText);
+  const colorRed = () => {
+    if (palos[palorandom] === "♥") {
+      topPalo.style.color = "red";
+      bottomPalo.style.color = "red";
+    } else if (palos[palorandom] === "♦") {
+      topPalo.style.color = "red";
+      bottomPalo.style.color = "red";
+    }
+  };
+  colorRed();
 };
+
+// &spades;
+// &clubs;
+// &hearts;
+// &diams;
